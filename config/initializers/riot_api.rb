95b3a9b4cd75e856bce741524d4112e3f66141f6
SimonBoeuf <simon.boeuf@gmail.com>
2015-08-27 17:30:04 +0200

    Ups

--- config/initializers/riot_api.rb
+++ config/initializers/riot_api.rb
@@ -1,4 +1,4 @@
-File.exists?(Rails.root.join("config/riot_api.yml")) ? RIOT_API_CONF = YAML.load_file(Rails.root.join("config/riot_api.yml"))[Rails.env] : nil
+File.exists?(Rails.root.join("config/riot_api.yml")) ? RIOT_API_CONF = YAML.load_file(Rails.root.join("config/riot_api.yml"))[Rails.env] : RIOT_API_CONF = {}
 API_KEY = RIOT_API_CONF['api_key'] || ENV['API_KEY']
 BASE_URL = RIOT_API_CONF['base_url'] || ENV['BASE_URL']
 TIME_OFFSET = RIOT_API_CONF['time_offset'] || ENV['TIME_OFFSET']
